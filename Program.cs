c30e28fc58dac0c4e90cf772d54081d6e624140a
rybanskanatalia <127695087+rybanskanatalia@users.noreply.github.com>
2025-06-08 17:54:35 +0200

    dynamic passenger pickup

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -8,37 +9,53 @@ class Program
         var map = new bool[10, 10];
         for (int x = 0; x < 10; x++)
             for (int y = 0; y < 10; y++)
-                map[x, y] = true; // all walkable for now
+                map[x, y] = true; // All walkable for now
 
         var pathfinder = new AStarPathfinder(10, 10, map);
         var planner = new RoutePlanner(pathfinder);
 
-        var passengers = new List<Passenger>
-        {
-            new Passenger(1, 2, 1, 3),
-            new Passenger(1, 1, 8, 8),
-            new Passenger(1, 4, 0, 0)
-        };
-
+        var passengers = new List<Passenger>();
         var start = (0, 0);
-        var result = planner.FindBestPath(start, passengers);
 
-        if (result.HasValue)
+        while (true)
         {
-            var (path, order) = result.Value;
-            Console.WriteLine("Passenger Order:");
-            foreach (var p in order)
-                Console.WriteLine($"{p.label} ({p.pos.x},{p.pos.y})");
+            Console.WriteLine("Enter pickup (x y) and dropoff (x y), or 'exit' (format = ' 1 1 1 1'):");
+            var input = Console.ReadLine();
 
-            Console.WriteLine("Full Path:");
-            foreach (var n in path)
-                Console.WriteLine($"({n.X}, {n.Y})");
+            if (input.ToLower() == "exit") break;
 
-            // TODO: Move Unity twin and send goals to ROS2
-        }
-        else
-        {
-            Console.WriteLine("No valid path found.");
+            var tokens = input.Split(' ');
+            if (tokens.Length != 4)
+            {
+                Console.WriteLine("Invalid input.");
+                continue;
+            }
+
+            var pickup = (int.Parse(tokens[0]), int.Parse(tokens[1]));
+            var dropoff = (int.Parse(tokens[2]), int.Parse(tokens[3]));
+
+            var newPassenger = new Passenger(pickup.Item1, pickup.Item2, dropoff.Item1, dropoff.Item2);
+            passengers.Add(newPassenger);
+
+            var result = planner.FindBestPath(start, passengers);
+
+            if (result.HasValue)
+            {
+                var (path, order) = result.Value;
+
+                Console.WriteLine("New passenger added.");
+                Console.WriteLine("Updated passenger order:");
+                foreach (var p in order)
+                    Console.WriteLine($"{p.label} ({p.pos.x},{p.pos.y})");
+
+                Console.WriteLine("Full path:");
+                foreach (var n in path)
+                    Console.WriteLine($"({n.X}, {n.Y})");
+            }
+            else
+            {
+                Console.WriteLine("No valid path found.");
+            }
         }
     }
 }
